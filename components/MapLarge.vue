cac85ecd7503c105b36cfd6b38dd77cd1d413a3c
mszob <72834440+mszob@users.noreply.github.com>
2021-07-29 00:31:17 -0500

    favicon background

---
+++ components/MapLarge.vue
@@ -0,0 +1,35 @@
+<template>
+<details>
+  <summary>Map</summary>
+<div class="language- exra-class">
+<pre class="language-text">
+<code>
+ Pre-tactical events
+ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+ TACTICAL PHASE
+,-------------------------------------.
+|  Pre-imp. events     Pr-im   Pr-im  |
+|  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀   ▀▀▀▀▀  |
+|  IMPULSE 1           IMP 2   IMP 3  |
+| ,-----------------. ,-----. ,-----. |
+| | Pre-gov-segment | | P-g | | P-g | |
+| | ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ | | ▀▀▀ | | ▀▀▀ | |
+| | GOV SEGMENT     | | GOV | | GOV | |
+| | ,-------------. | | ,-. | | ,-. | |
+| | |  - Movement | | | |-| | | |-| | |
+| | |  - Battle   | | | |-| | | |-| | |
+| | `-------------' | | `-' | | `-' | |
+| | Pre-reb-segment | | P-r | | P-r | |
+| | ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ | | ▀▀▀ | | ▀▀▀ | |
+| | REBEL SEGMENT   | | REB | | REB | |
+| | ,-------------. | | ,-. | | ,-. | |
+| | |  - Movoment | | | |-| | | |-| | |
+| | |  - Battle   | | | |-| | | |-| | |
+| | `-------------' | | `-' | | `-' | |
+| `-----------------' `-----' `-----' |
+`-------------------------------------'
+</code>
+</pre>
+</div>
+</details>
+</template>
---
+++ components/MapSmall.vue
@@ -0,0 +1,40 @@
+<template>
+<details>
+  <summary>Map</summary>
+<div class="language- exra-class">
+<pre class="language-text">
+<code>
+     Pre-tactical
+     █████████████████████
+     TACTICAL PHASE
+    ,---------------------.
+    |  Pre-impulse        |
+    |  █████████████████  |
+    |  IMPULSE 1          |
+    | ,-----------------. |
+    | | Pre-gov-segment | |
+    | | ███████████████ | |
+    | | GOV SEGMENT     | |
+    | | ,-------------. | |
+    | | |  - Movement | | |
+    | | |  - Battle   | | |
+    | | `-------------' | |
+    | | Pre-reb-segment | |
+    | | ███████████████ | |
+    | | REBEL SEGMENT   | |
+    | | ,-------------. | |
+    | | |  - Movoment | | |
+    | | |  - Battle   | | |
+    | | `-------------' | |
+    | `-----------------' |
+    |                     |
+    |  IMPULSE 2...       |
+    |                     |
+    |  IMPULSE 3...       |
+    |                     |
+    `---------------------'
+</code>
+</pre>
+</div>
+</details>
+</template>
